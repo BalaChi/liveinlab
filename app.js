0a9cd7b03978864d31321acc6a706153c816a460
balachi <balachandhar2003jp@gmail.com>
2022-04-09 14:19:00 +0530

    added procfile

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require("express")
 const app = express()
 
+//app.get('/',function(req,res){
+//    res.send("running hello babe!")
+//})
+
+//app.listen(9000,function(req,res) {
+ //   console.log("hello")
+//})
+
+
 app.use(express.static("public"))
 
 app.get("/", function (req, res) {
     res.send("<h1>Hello World!</h1>")})
 
+    
 app.listen(process.env.PORT || 3000, () => console.log("server is running"));
